e25d6ca6182abf4edffa441f1fbb6186333d838b
dav <usr21p1@gmail.com>
2025-02-16 17:43:05 -0800

    Axe Validation

--- wgpu-core/src/command/bind.rs
+++ wgpu-core/src/command/bind.rs
@@ -462,6 +462,7 @@ impl Binder {
         &self,
         pipeline: &T,
     ) -> Result<(), Box<BinderError>> {
+        return Ok(());
         self.manager.get_invalid().map_err(|(index, error)| {
             Box::new(match error {
                 compat::Error::Incompatible {
